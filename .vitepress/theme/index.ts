ef0f4465d4adfac9847603b9e7a9e3bac74478d5
Thiago Oliveira <skepvox@gmail.com>
2026-06-13 15:27:38 -0300

    Remove dead Vue-docs components and assets

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -2,10 +2,6 @@ import './ssr-shim'
 import { h, App } from 'vue'
 import { VPTheme } from '@vue/theme'
 import './styles/index.css'
-import VueSchoolLink from './components/VueSchoolLink.vue'
-import ScrimbaLink from './components/ScrimbaLink.vue'
-// import Banner from './components/Banner.vue'
-// import TextAd from './components/TextAd.vue'
 import NavBarTitleBrand from './components/NavBarTitleBrand.vue'
 
 import 'vitepress/dist/client/theme-default/styles/components/vp-code-group.css'
@@ -15,13 +11,7 @@ export default Object.assign({}, VPTheme, {
   Layout: () => {
     // @ts-ignore
     return h(VPTheme.Layout, null, {
-      // banner: () => h(Banner),
       'navbar-title': () => h(NavBarTitleBrand)
     })
-  },
-  enhanceApp({ app }: { app: App }) {
-    app.component('VueSchoolLink', VueSchoolLink)
-    app.component('ScrimbaLink', ScrimbaLink)
-    // app.component('TextAd', TextAd)
   }
 })
